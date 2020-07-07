9bda7599b975136fbcd545a7bcb2ea57717a4510
JACPro <jamesacpro@gmail.com>
2020-07-07 16:22:36 +0100

    Added persistent music, moving camera and basic ship left/right

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake() {
+        DontDestroyOnLoad(gameObject);   
+    }
+
+    void Start()
+    {
+        StartCoroutine(WaitToLoad());
+    }
+
+    IEnumerator WaitToLoad() 
+    {
+        yield return new WaitForSeconds (3f);
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In (m/s)^-1")][SerializeField] float xSpeed = 4f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -3, 3);
+        
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    }
+}
