d1bd98fb30eaeb2e0353c1cd4e08be896c674b89
JACPro <jamesacpro@gmail.com>
2020-12-15 16:14:51 +0000

    Organised files and refactored scene loading out of MusicPlayer into its own script

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -8,15 +8,4 @@ public class MusicPlayer : MonoBehaviour
     void Awake() {
         DontDestroyOnLoad(gameObject);   
     }
-
-    void Start()
-    {
-        StartCoroutine(WaitToLoad());
-    }
-
-    IEnumerator WaitToLoad() 
-    {
-        yield return new WaitForSeconds (3f);
-        SceneManager.LoadScene(1);
-    }
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(WaitToLoad());
+    }
+
+    IEnumerator WaitToLoad()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+}
+
