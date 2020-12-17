ee183181fb87ca720da7a0b02f249d944d0da857
JACPro <jamesacpro@gmail.com>
2020-12-17 23:49:04 +0000

    Play explosion FX on player collision and reload scene

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")] [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,5 +17,12 @@ public class CollisionHandler : MonoBehaviour
     {
         print("Player died!");
         SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);        
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
