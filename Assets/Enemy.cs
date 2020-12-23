043f942deb0a010842c220d4c2f08599f2956d4c
JACPro <jamesacpro@gmail.com>
2020-12-23 22:13:37 +0000

    Enemies now add their own box collider at runtime

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,9 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    void Start() {
+        AddBoxCollider();
+    }
+
+    /*
+    Box collider added at runtime so that if the asset pack is updated and components we've added 
+    are removed, we won't run into hit detection issues.
+    NOTE: No trigger on added box collider
+    */
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false; 
+    }
+
     private void OnParticleCollision(GameObject other) {
         Destroy(gameObject);
     }
