c709948ed0896b5f0159a7260122f4de7afdf3fa
JACPro <jamesacpro@gmail.com>
2021-01-18 20:40:29 +0000

    Added scoreboard class for updating score UI

---
+++ Assets/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 10;
+
+    int score;
+    Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateScoreText();
+    }
+
+    public void ScoreHit() 
+    {
+        score += scorePerHit;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "SCORE:   " + score;
+    }
+}
